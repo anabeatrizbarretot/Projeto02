a68975619fd9ba0acc145a45d9313890d07178b4
ana beatriz barreto <anabeatrizbarretot@gmail.com>
2025-06-30 10:59:23 -0300

    ,
    ,

---
+++ script.js
@@ -0,0 +1,11 @@
+function abrirFormulario() {
+  window.open("formulario.html", "_blank");
+}
+
+function abrirFrutas() {
+  window.open("frutas.html", "_blank");
+}
+
+function abrirJogo() {
+  window.open("jogo.html", "_blank");
+}
